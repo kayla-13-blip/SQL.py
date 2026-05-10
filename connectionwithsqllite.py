edf872ef49251dd751d86925cd3956cc2b23bd6d
kayla-13-blip <SholotanOdunayo13@gmail.com>
2026-05-10 20:02:39 +0100

    Add files via upload

---
+++ connectionwithsqllite.py
@@ -0,0 +1,13 @@
+"""
+# Import file from your system from google.colab import files
+file = files.upload()
+"""
+import sqlite3
+database = 'database.sqlite'
+conn = sqlite3.connect(database)
+print('Opened data sucessfully')
+import pandas as pd
+tables = pd.read_sql("""SELECT * 
+                     FROM sqlite_master
+                     WHERE type = 'table';""", conn)
+print(tables)
